61f7b796e61931660d300ac3c186c79daf43c77d
Victor Hugo Montano <victor.montano@assuresoft.com>
2022-03-04 17:15:37 -0400

    task[001]: Create spring proyect

---
+++ beacon/src/test/java/com/example/beacon/BeaconApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.beacon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeaconApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
