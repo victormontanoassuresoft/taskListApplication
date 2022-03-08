1b6a92ec3bd6aa9fae12d400e4497093480f36a3
Victor Hugo Montano <victor.montano@assuresoft.com>
2022-03-07 22:35:56 -0400

    task[001]: Add task service methods

---
+++ beacon/src/main/java/com/example/beacon/service/TaskService.java
@@ -0,0 +1,52 @@
+package com.example.beacon.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.beacon.persistence.Task;
+import com.example.beacon.persistence.repository.TaskRepository;
+
+@Service
+public class TaskService {
+	
+	@Autowired
+	private TaskRepository taskRepository;
+	
+	public Task createTask (Task task) {
+		return taskRepository.save(task);
+	}
+	
+	public List<Task> listAllTask (){
+		return taskRepository.findAll();
+	}
+	
+	public void deleteTask (Long taskId) {
+		taskRepository.deleteById(taskId);;
+	}
+	
+	public Optional<Task> findTaskByID (Long taskId) {
+		return taskRepository.findById(taskId);
+	}
+	
+	public Task updateTask (Task task) {
+		return taskRepository.save(task);
+	}
+	
+	public Task updateComplete (Long taskId, Boolean completed) {
+		Optional<Task> optionalTaskToUpdate = taskRepository.findById(taskId);
+		Task taskToUpdate = optionalTaskToUpdate.get();
+		taskToUpdate.setCompleted(completed);
+		return taskRepository.save(taskToUpdate);
+	}
+	
+	public Task updateTitle (Long taskId, String title) {
+		Optional<Task> optionalTaskToUpdate = taskRepository.findById(taskId);
+		Task taskToUpdate = optionalTaskToUpdate.get();
+		taskToUpdate.setTitle(title);
+		return taskRepository.save(taskToUpdate);
+	}
+
+}
