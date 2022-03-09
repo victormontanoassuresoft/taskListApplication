bf609327a9b576b28e6a4ffb04e235bed9349938
Victor Hugo Montano <victor.montano@assuresoft.com>
2022-03-09 17:48:52 -0400

    task[001]: Move task exit dto file

--- beacon/src/main/java/com/example/beacon/controller/api/request/TaskEntryDto.java
+++ beacon/src/main/java/com/example/beacon/controller/api/request/TaskEntryDto.java
@@ -1,5 +1,13 @@
 package com.example.beacon.controller.api.request;
 
-public class TaskEntryDto {
+import java.util.Date;
 
+import lombok.Data;
+
+@Data
+public class TaskEntryDto {
+    
+    private String title;
+    private Boolean completed;
+    private Date dueDate;
 }
