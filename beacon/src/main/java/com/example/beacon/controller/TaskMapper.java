b484b43ba2a5ded34276d5a6c02e064b953d7d9c
Victor Hugo Montano <victor.montano@assuresoft.com>
2022-03-09 17:55:25 -0400

    task[001]: Add task mapper interface

---
+++ beacon/src/main/java/com/example/beacon/controller/TaskMapper.java
@@ -0,0 +1,17 @@
+package com.example.beacon.controller;
+
+import java.util.List;
+
+import org.mapstruct.Mapper;
+
+import com.example.beacon.controller.api.response.TaskExitDto;
+import com.example.beacon.controller.api.request.TaskEntryDto;
+import com.example.beacon.persistence.Task;
+
+@Mapper
+public interface TaskMapper {
+	
+	public TaskExitDto entityToDto(Task task);
+	public List<TaskExitDto> entityToDto(List<Task> tasks);
+	public Task dtoToEntity(TaskEntryDto dto);
+}
