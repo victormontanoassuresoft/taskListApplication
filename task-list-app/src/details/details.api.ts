dc0019af3806e5635783fa7dd3e7aebdc9ca6e5f
Victor Hugo Montano <victor.montano@assuresoft.com>
2022-03-11 11:35:23 -0400

    task[001]: Add details api

---
+++ task-list-app/src/details/details.api.ts
@@ -0,0 +1,30 @@
+import { AxiosResponse } from "axios";
+import Api from "../common/api";
+import { Task } from "../common/types";
+
+const getTask = async(taskId: number): Promise<Task> => {
+  const { data } = await Api.get<Task>(`/${taskId}`);
+  return data;
+}
+
+const updateTask = async (task: Task): Promise<Task> => {
+  const { data } = await Api.patch<Task, Task>(`/${task.taskId}`, task);
+  return data
+}
+
+const createTask = async (task: Task): Promise<Task> => {
+  const { data } = await Api.post<Task, Task>(`/create`, task);
+  return data;
+}
+
+const deleteTask = async (taskId: number) => {
+  const { data } = await Api.delete<Task[]>(`/${taskId}`);
+  return data
+}
+
+export {
+    getTask,
+    updateTask,
+    createTask,
+    deleteTask
+}
