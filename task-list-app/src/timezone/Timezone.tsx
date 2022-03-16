1d960a15e15e650a101a86af86676fb349dce5ef
Victor Hugo Montano <victor.montano@assuresoft.com>
2022-03-16 15:58:57 -0400

    task[001]: Update form of coordinates

--- task-list-app/src/timezone/Timezone.tsx
+++ task-list-app/src/timezone/Timezone.tsx
@@ -2,11 +2,10 @@ import React, { FormEventHandler, useEffect, useState } from 'react'
 import { navigate, RouteComponentProps } from '@reach/router';
 import { Box, Button, makeStyles, TextField } from '@material-ui/core';
 import { Timezone } from '../common/timezone';
-import { Coordinate } from '../common/coordenate';
 import { getTimezone } from './timezone.api';
 
 interface TimezoneProps extends RouteComponentProps {
-    coordinate?: Coordinate;
+  coordinate?: string;
 }
 
 const useStyles = makeStyles({
@@ -39,34 +38,21 @@ const TimezonePage: React.FC<TimezoneProps> = ({coordinate}) => {
 
     const onFormSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
         e.preventDefault();
-       
-    }
+        navigate(`/timezone/${coordinate}`);
 
-    const onEdit = (coordinate: Coordinate) => navigate(`timezone`);
+    }
 
     const onFormValueChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setTimezone(prevValue => {
-          if(prevValue) {
-            return {
-              ...prevValue,
-              [e.target.name]: e.target.value
-            } as Timezone
-          } else {
-            return {
-              [e.target.name]: e.target.value
-            } as unknown as Timezone
-          }
-        })
-    }
+      coordinate = e.target.value;
+  }
     
     return (
         <div>
             <a href="https://timezonedb.com/">Time zone</a>
             <form onSubmit={onFormSubmit}>
                 <Box display="flex" alignItems="center" justifyContent="center" flexDirection="column">
-                    <TextField onChange={onFormValueChange} className={classes.spacing} name="lat" label="Latitude" placeholder="Latitude"/>
-                    <TextField onChange={onFormValueChange} className={classes.spacing} name="lgn" label="Longitude" placeholder="Longitude"/>
-                    <Button className={classes.spacing} type="submit" variant="contained" color="primary">Submit</Button>
+                    <TextField onChange={onFormValueChange} className={classes.spacing} name="coordinate" label="Coordinates" placeholder="-17.414,-66.1653"/>
+                    <Button className={classes.spacing} type="submit" variant="contained" color="primary">Get time zone</Button>
                 </Box>
             </form>
             <Box display="flex" alignItems="center" justifyContent="center" flexDirection="column">
