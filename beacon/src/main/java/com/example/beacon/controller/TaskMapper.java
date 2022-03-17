4dbe74c12c85bfa2718cace24b2c74e957f903b4
Victor Hugo Montano <victor.montano@assuresoft.com>
2022-03-17 09:46:32 -0400

    task[001]: Update task mapper

--- beacon/src/main/java/com/example/beacon/controller/TaskMapper.java
+++ beacon/src/main/java/com/example/beacon/controller/TaskMapper.java
@@ -4,14 +4,14 @@ import java.util.List;
 
 import org.mapstruct.Mapper;
 
-import com.example.beacon.controller.api.response.TaskExitDto;
-import com.example.beacon.controller.api.request.TaskEntryDto;
+import com.example.beacon.controller.api.request.CreateTaskRequest;
+import com.example.beacon.controller.api.response.TaskResponse;
 import com.example.beacon.persistence.Task;
 
 @Mapper(componentModel = "spring")
 public interface TaskMapper {
 	
-	public TaskExitDto entityToDto(Task task);
-	public List<TaskExitDto> entityToDto(List<Task> tasks);
-	public Task dtoToEntity(TaskEntryDto dto);
+	public TaskResponse entityToDto(Task task);
+	public List<TaskResponse> entityToDto(List<Task> tasks);
+	public Task dtoToEntity(CreateTaskRequest dto);
 }
