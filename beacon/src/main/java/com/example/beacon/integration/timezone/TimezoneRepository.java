089c16581c608256644a6c132ba54b140e5dc4a8
Victor Hugo Montano <victor.montano@assuresoft.com>
2022-03-16 22:03:29 -0400

    task[001]: Move task repository'

--- beacon/src/main/java/com/example/beacon/integration/timezone/TimezoneRepository.java
+++ beacon/src/main/java/com/example/beacon/integration/timezone/TimezoneRepository.java
@@ -1,11 +1,11 @@
-package com.example.beacon.persistence.repository;
+package com.example.beacon.integration.timezone;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.example.beacon.persistence.Timezone;
 
-@FeignClient(value="getTimezone", url="http://api.timezonedb.com/v2.1/get-time-zone?key=VAQC6KNHS0BR&format=json&by=position")
+@FeignClient(value="getTimezone", url="http://api.timezonedb.com/v2.1/get-time-zone?key=VAQC6KNHS0BR&format=json&by=position&lat=-17.414&lng=-66.1653")
 public interface TimezoneRepository {
 
 	@GetMapping()
