67de260b2eff99023d959a999f136205d8c681ef
Victor Hugo Montano <victor.montano@assuresoft.com>
2022-03-20 21:45:19 -0400

    task[001]: Add reducer and store function

--- task-list-app/src/App.tsx
+++ task-list-app/src/App.tsx
@@ -3,12 +3,35 @@ import { Router } from '@reach/router';
 import Home from './home/Home';
 import Details from './details/Details';
 import Timezone from './timezone/Timezone';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { Task } from './common/types';
+import { getTaskList } from './home/home.api';
+
+const initialState = {
+  tasks: []
+}
+
+function reducer(state = initialState, action) {
+  switch(action.type) {
+    case 'GETLIST':
+      return {
+        tasks: state.tasks = getTaskList()
+      }
+    default:
+      return state;
+  }
+  return state
+}
+const store = createStore(reducer);
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Home path="/"/>
+        <Provider store={store}>
+          <Home path="/"/>
+        </Provider>
         <Details path="/task/:taskId"/>
         <Details path="/task/create" /> 
         <Timezone path="/timezone/:coordinate" />
